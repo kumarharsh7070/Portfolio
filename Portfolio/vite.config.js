c2ca8c45d94df695db3bb1566e38987d5c38229b
kumar harsh <hk995814@gmail.com>
2026-01-14 21:41:15 +0530

    Clean project structure and remove tailwind vite plugin

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,5 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default {
-  plugins: [tailwindcss()],
-};
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
